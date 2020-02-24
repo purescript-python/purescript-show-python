ed4e4be5cb222357980096e650416c64dc90dd7a
thautawarm <twshere@outlook.com>
2020-02-25 00:17:34 +0900

    learning spago config from purescript-dotenv

--- spago.dhall
+++ spago.dhall
@@ -1,10 +1,8 @@
-{-
-Welcome to a Spago project!
-You can edit this file as you like.
--}
-{ name = "my-project"
-, dependencies = [ "console", "effect", "prelude", "psci-support" ]
+{ name = "show-python"
+, dependencies = [ "console", "effect", "prelude" ]
 , packages = ./packages.dhall
 , sources = [ "src/**/*.purs", "test/**/*.purs" ]
 , backend = "pspy"
+, license = "MIT"
+, repository = "https://github.com/purescript-python/purescript-show-python"
 }
--- src/Python/IO/Unsafe.purs
+++ src/Python/IO/Unsafe.purs
@@ -11,15 +11,21 @@ import Prelude
 
 import Effect (Effect)
 
+-- Equal to `str` in Python but wrapped in Effect monad.
 foreign import show :: forall a. a -> Effect String
 
+-- Equal to `print` in Python but wrapped in Effect monad.
 foreign import printLn :: forall a. a -> Effect Unit
 
+-- Equal to `repr` in Python but wrapped in Effect monad.
 foreign import repr :: forall a. a -> Effect String
 
+-- Equal to `assert cond, msg` in Python but wrapped in Effect monad.
 foreign import assertMsg :: Boolean -> String -> Effect Unit
 
+-- Equal to `assert cond` in Python but wrapped in Effect monad.
 foreign import assert_ :: Boolean -> Effect Unit
 
+-- See assert_
 assert :: Boolean -> Effect Unit
 assert = assert_
